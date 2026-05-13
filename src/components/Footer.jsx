d4e443271d35e68f87d76d0580c1524767626a80
ysf-rhazzal <143925916+ysf-rhazzal@users.noreply.github.com>
2026-05-14 00:47:48 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   useEffect(() => {
     const fetchSettings = async () => {
       try {
-        const response = await axios.get('https://8be10083-1e37-47e1-9fce-56446b72d950-00-3uxap2afpc8tk.janeway.replit.dev/api/settings');
+        const response = await axios.get('https://achbalsportive--youssefrhazzal9.replit.app/api/settings');
         setSettings(Array.isArray(response.data) ? response.data[0] : response.data);
       } catch (error) {
         console.error('Error fetching settings for footer:', error);
