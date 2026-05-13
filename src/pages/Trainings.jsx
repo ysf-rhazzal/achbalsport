1ad9a11eaf8686e59e4b3acf155c29eb81802b2a
ysf-rhazzal <143925916+ysf-rhazzal@users.noreply.github.com>
2026-05-14 00:44:11 +0100

    Update Trainings.jsx

--- src/pages/Trainings.jsx
+++ src/pages/Trainings.jsx
@@ -8,7 +8,7 @@ const Trainings = () => {
   useEffect(() => {
     const fetchTrainings = async () => {
       try {
-        const response = await axios.get('https://8be10083-1e37-47e1-9fce-56446b72d950-00-3uxap2afpc8tk.janeway.replit.dev/api/trainings');
+        const response = await axios.get('https://achbalsportive--youssefrhazzal9.replit.app/api/trainings');
         setTrainings(response.data);
       } catch (error) {
         console.error('Error fetching trainings:', error);
