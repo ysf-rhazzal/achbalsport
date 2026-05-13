24a8abe758a55fc4ec796b034a614d4aeedf9cd5
ysf-rhazzal <143925916+ysf-rhazzal@users.noreply.github.com>
2026-05-14 00:41:13 +0100

    Update NewsDetails.jsx

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -10,7 +10,7 @@ const NewsDetails = () => {
   useEffect(() => {
     const fetchItem = async () => {
       try {
-        const response = await axios.get(`https://8be10083-1e37-47e1-9fce-56446b72d950-00-3uxap2afpc8tk.janeway.replit.dev/api/news/${id}`);
+        const response = await axios.get(`https://achbalsportive--youssefrhazzal9.replit.app/api/news/${id}`);
         setItem(response.data);
       } catch (error) {
         console.error('Error fetching details:', error);
